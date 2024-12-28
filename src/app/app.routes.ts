859c70d185cc5618ea905b83abe1de4d846cef0c
aarti <aartinarwade2002@gmail.com>
2024-12-28 15:53:54 +0530

    navbar added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './navbar/navbar.component';
 
 export const routes: Routes = [
-  {path:'',redirectTo:'navbar',pathMatch:'full'},
+  {path:'',redirectTo:'home',pathMatch:'full'},
   {path:'home',component:HomeComponent},
   {path:'navbar',component:NavbarComponent}
 ];
