7af766ad44187a95841f95dd0326095a55a9c1f6
Sandip Chavan <chavansandip4172@gmail.com>
2024-12-28 16:10:06 +0530

    add aboutus component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,18 +1,9 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-<<<<<<< HEAD
 import { AboutusComponent } from './aboutus/aboutus.component';
 
 export const routes: Routes = [
   {path:'',redirectTo:'home',pathMatch:'full'},
   {path:'home',component:HomeComponent},
   {path:'aboutus',component:AboutusComponent}
-=======
-import { NavbarComponent } from './navbar/navbar.component';
-
-export const routes: Routes = [
-  {path:'',redirectTo:'navbar',pathMatch:'full'},
-  {path:'home',component:HomeComponent},
-  {path:'navbar',component:NavbarComponent}
->>>>>>> 25d30f00cdbe09f1753e074d9120ff0b35b11eda
 ];
