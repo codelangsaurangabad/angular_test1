d0b4ec7234f650c0fb5b467393722fd50ca66090
rushikeshdapke <rushikeshkdapke@example.com>
2025-01-15 13:27:37 +0530

    created About us page and added common css and js file

---
+++ public/js/common.style.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const heading = document.querySelector('.animated-heading');
+  if (heading) {
+    heading.classList.add('visible');
+  }
+
+});
