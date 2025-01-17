4abb79769eccb96f54bb128057063686f916a724
rushikeshdapke <rushikeshkdapke@example.com>
2025-01-17 09:08:18 +0530

    created contact us page and added enviroment variables and data binded through it

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { environment } from '../../environments/environment.development';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  imports: [MatIconModule,FormsModule,CommonModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
 export class ContactComponent {
+  address = environment.flyviewDetails.address
+contact = environment.flyviewDetails.contact
+email = environment.flyviewDetails.email
 
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,1 +1,7 @@
-export const environment = {};
+export const environment = {
+  flyviewDetails : {
+    contact : '+919096244262',
+    address : 'Plot No. 32, CTS No. 16295/34, Sheet no 183, Jyoti Nagar, Shahnoorwadi, Behind Sai mandir, Aurangabad-431001',
+    email : 'flyviewghis@gmail.com'
+  }
+};
